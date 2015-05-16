66750b3cdffd9f4f6b968a81141bf29f668f9ffd
bellmounte <ryan.dacquisto@gmail.com>
2015-05-15 21:07:47 -0700

    Adding movement directions for `RIGHT` `LEFT` and `DOWN`.

--- src/core/game_board.cpp
+++ src/core/game_board.cpp
@@ -75,30 +75,55 @@ bool GameBoard::perform_move_vertical(Direction direction)
 
 	// Perform transforms
 	for (int col = 0; col < COLUMNS; col++) {
+		if (direction == UP) {
+			for (int row = 0; row < ROWS; row++) {
+				// If the slot is empty, continue.
+				if (board[row][col].isEmpty()) {
+					continue;
+				}
 
-		for (int row = 0; row < ROWS; row++) {
+				int current_row = row;
 
-			// If the slot is empty, continue.
-			if (board[row][col].isEmpty()) {
-				continue;
+				while (row + 1 < ROWS) {
+
+					int next_row = row + 1;
+
+					if (board[next_row][col].isEmpty()) {
+						// If the slot is empty, continue.
+						row++;
+					} else if (board[current_row][col].hasSameValueTile(board[next_row][col])) {
+						board[current_row][col].doubleTile();
+						board[next_row][col].removeTile();
+						valid_move = true;
+						break;
+					} else {
+						break;
+					}
+				}
 			}
+		} else if (direction == DOWN) {
+			for (int row = ROWS - 1; row > 0; row--) {
+				// If the slot is empty, continue.
+				if (board[row][col].isEmpty()) {
+					continue;
+				}
 
-			int current_row = row;
+				int current_row = row;
 
-			while (row + 1 < ROWS) {
+				while (row - 1 >= 0) {
+					int next_row = row - 1;
 
-				int next_row = row + 1;
-
-				if (board[next_row][col].isEmpty()) {
-					// If the slot is empty, continue.
-					row++;
-				} else if (board[current_row][col].hasSameValueTile(board[next_row][col])) {
-					board[current_row][col].doubleTile();
-					board[next_row][col].removeTile();
-					valid_move = true;
-					break;
-				} else {
-					break;
+					if (board[next_row][col].isEmpty()) {
+						// If the slot is empty, continue.
+						row--;
+					} else if (board[current_row][col].hasSameValueTile(board[next_row][col])) {
+						board[current_row][col].doubleTile();
+						board[next_row][col].removeTile();
+						valid_move = true;
+						break;
+					} else {
+						break;
+					}
 				}
 			}
 		}
@@ -123,6 +148,20 @@ bool GameBoard::perform_move_vertical(Direction direction)
 					valid_move = true;
 				}
 			}
+		} else if (direction == DOWN) {
+			for (int row = ROWS - 1; row >= 0; row--) {
+				if (board[row][col].isEmpty()) {
+					// Set first available index if one does not exist.
+					if (available_row == -1) {
+						available_row = row;
+					}
+				} else if (available_row != -1) {
+					board[available_row][col].setTile(board[row][col].getTile().getValue());
+					board[row][col].removeTile();
+					available_row--;
+					valid_move = true;
+				}
+			}
 		}
 	}
 
@@ -132,8 +171,99 @@ bool GameBoard::perform_move_vertical(Direction direction)
 bool GameBoard::perform_move_horizontal(Direction direction)
 {
 	bool valid_move = false;
-	for (int i = 0; i < ROWS; i++) {
+
+	// Perform transforms
+	for (int row = 0; row < ROWS; row++) {
+		if (direction == LEFT) {
+			for (int col = 0; col < ROWS; col++) {
+				// If the slot is empty, continue.
+				if (board[row][col].isEmpty()) {
+					continue;
+				}
+
+				int current_col = col;
+
+				while (col + 1 < COLUMNS) {
+
+					int next_col = col + 1;
+
+					if (board[row][next_col].isEmpty()) {
+						// If the slot is empty, continue.
+						col++;
+					} else if (board[row][current_col].hasSameValueTile(board[row][next_col])) {
+						board[row][current_col].doubleTile();
+						board[row][next_col].removeTile();
+						valid_move = true;
+						break;
+					} else {
+						break;
+					}
+				}
+			}
+		} else if (direction == RIGHT) {
+			for (int col = COLUMNS - 1; col > 0; col--) {
+				// If the slot is empty, continue.
+				if (board[row][col].isEmpty()) {
+					continue;
+				}
+
+				int current_col = col;
+
+				while (col - 1 >= 0) {
+					int next_col = col - 1;
+
+					if (board[row][next_col].isEmpty()) {
+						// If the slot is empty, continue.
+						col--;
+					} else if (board[row][current_col].hasSameValueTile(board[row][next_col])) {
+						board[row][current_col].doubleTile();
+						board[row][next_col].removeTile();
+						valid_move = true;
+						break;
+					} else {
+						break;
+					}
+				}
+			}
+		}
 	}
+
+	// Perform Shifts
+	for (int row = 0; row < ROWS; row++) {
+
+		int available_col = -1;
+
+		if (direction == LEFT) {
+			for (int col = 0; col < COLUMNS; col++) {
+				if (board[row][col].isEmpty()) {
+					// Set first available index if one does not exist.
+					if (available_col == -1) {
+						available_col = col;
+					}
+				} else if (available_col != -1) {
+					board[row][available_col].setTile(board[row][col].getTile().getValue());
+					board[row][col].removeTile();
+					available_col++;
+					valid_move = true;
+				}
+			}
+		} else if (direction == RIGHT) {
+			for (int col = COLUMNS - 1; col >= 0; col--) {
+				if (board[row][col].isEmpty()) {
+					// Set first available index if one does not exist.
+					if (available_col == -1) {
+						available_col = col;
+					}
+				} else if (available_col != -1) {
+					board[row][available_col].setTile(board[row][col].getTile().getValue());
+					board[row][col].removeTile();
+					available_col--;
+					valid_move = true;
+				}
+			}
+		}
+	}
+
 	return valid_move;
 }
 
