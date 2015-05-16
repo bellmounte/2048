ab63885d634913cf32393229790f9bbf17821384
bellmounte <ryan.dacquisto@gmail.com>
2015-05-16 13:48:03 -0700

    Switching to using a more convenient input method.

--- src/platforms/terminal/term_game.cpp
+++ src/platforms/terminal/term_game.cpp
@@ -7,22 +7,61 @@
 
 Direction TermGame::getInput()
 {
-	std::cout << "Enter the shift direction [`up`, `down`, `left`, or `right`]: ";
-	std::string input = "";
-	getline(std::cin, input);
+	Direction result;
+	char pressed_key;
+    int ascii_code = 0;
+    bool useEscapeSequence = false;
 
-	if (input == "up" || input == "u") {
-		return UP;
-	} else if (input == "down" || input == "d") {
-		return DOWN;
-	} else if (input == "left" || input == "l") {
-		return LEFT;
-	} else if (input == "right" || input == "r") {
-		return RIGHT;
-	} else {
-		std::cout << "Invalid input: [" << input << "], try again:" << std::endl;
-		return getInput();
-	}
+    while(ascii_code != 3) {
+        system("/bin/stty raw");
+        pressed_key = getchar();
+        system("/bin/stty cooked");
+        ascii_code = pressed_key;
+
+        if (ascii_code == 27) {
+        	useEscapeSequence = true;
+        } else if (useEscapeSequence) {
+        	// Arrow Keys
+        	switch (ascii_code) {
+        		case 65:
+        			return UP;
+        		case 66:
+        			return DOWN;
+        		case 67:
+        			return RIGHT;
+        		case 68:
+        			return LEFT;
+        		case 27:
+        		case 91:
+        			continue;
+        		default:
+        			useEscapeSequence = false;
+        	}
+        } else {
+        	switch (ascii_code) {
+        		// A
+        		case 97:
+        		case 65:
+        			return LEFT;
+        		// S
+        		case 115:
+        		case 83:
+        			return DOWN;
+        		// W
+        		case 119:
+        		case 87:
+        			return UP;
+        		// D
+        		case 100:
+        		case 86:
+        			return RIGHT;
+        	}
+        }
+    }
+
+    // Returning an undefined Direction object to make the compiler happy.
+    // Note: I assume there is a chance that this will possibly at some point return a valid Direction.
+    return result;
 }
 
 void TermGame::start()
@@ -37,11 +76,18 @@ void TermGame::start()
 		// Get user input for next move.
 		Direction input = getInput();
 
-		// Perform a move based on the given user input.
-		game_board.perform_move(input);
+		if (input > 3) {
+			// Handle the case where no valid input is was present. (ctrl + c)
+			in_progress = false;
+		} else {
+			// Perform a move based on the given user input.
+			game_board.perform_move(input);
 
-		// Check to see if there are any valid moves left.
-		in_progress = !(game_board.is_game_over());
-
+			// Check to see if there are any valid moves left.
+			in_progress = !(game_board.is_game_over());
+		}
 	}
+	// Print the final 2048 Board
+	game_board.print();
+
 }
